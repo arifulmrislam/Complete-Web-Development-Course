c703a3ca93711306bcb9a3ec79f779a15f8b62ab
arifulmrislam <ariful.mr.islam@gmail.com>
2024-01-27 13:50:35 +0600

    if you enter over amount from balance than we will get alert

--- Milestone 5 Integrate JavaScript/Module 27 Simple Interactive Bank website/Baap Er Bank/js/withdraw.js
+++ Milestone 5 Integrate JavaScript/Module 27 Simple Interactive Bank website/Baap Er Bank/js/withdraw.js
@@ -18,23 +18,41 @@ document.getElementById("btn-withdraw").addEventListener('click',function(){
     const newWithDrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithDrawAmountString);
 
+    //step7:
+    withdrawField.value = '';
+
+
+    if (isNaN(newWithdrawAmount)){
+        alert('Please enter the valid amount');
+        return;
+    }
+
     //step 3: 
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    //step4:
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
     
     //step 5:
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+
+
+
+
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert("Card decline. Have a look your account balance");
+        return;
+    }
+    
+    //step4:
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
     
     //step 6: 
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
-    //step7:
-    withdrawField.value = '';
+
 })
