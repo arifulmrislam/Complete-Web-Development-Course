48e3952ae05e30599b9c8a05b00261f8e92647cb
arifulmrislam <ariful.mr.islam@gmail.com>
2024-03-04 04:24:47 +0600

    Module 41

---
+++ Milestone 7 Introduction to simple React/Module 41/es6_module_overview/src/Components/Sunglass/Sunglass.jsx
@@ -0,0 +1,19 @@
+import './Sunglass.css'
+import Watch from '../../Watch/Watch';
+import { add, multiply, dividedTheFirstNumberByTheSecondNumbers as divided } from '../../Calculator/Calculator';
+
+const Sunglass = () => {
+    const first = 50;
+    const second = 30;
+    const sum = add(first + second);
+    const mul = multiply(first * second);
+    const div = divided( first / second);
+
+    return (
+        <div>
+            <Watch></Watch>
+        </div>
+    );
+};
+
+export default Sunglass;
---
+++ Milestone 7 Introduction to simple React/Module 41/memorable_water_bottle/src/Components/Utilites/localStorage.jsx
@@ -0,0 +1,27 @@
+const getStoreCart = () => {
+    const storedCardString = localStorage.getItem('cart');
+    if(storedCardString) {
+        return JSON.parse(storedCardString);
+    }
+    return [];
+}
+
+const saveCartToLS = cart => {
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+const addToLs = id => {
+    const cart = getStoreCart();
+    cart.push(id);
+    //save to local storage
+    saveCartToLS(cart);
+}
+
+const removeFromLS = id => {
+    const cart = getStoreCart();
+    //removing every id
+    const remaining = cart.filter(idx => idx !== id);
+    saveCartToLS(remaining);
+}
+export{addToLs, getStoreCart, removeFromLS};
