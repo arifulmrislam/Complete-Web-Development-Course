03f71a1b8dca4c5e2d15e3ad2ca93905e4e5b1e2
arifulmrislam <ariful.mr.islam@gmail.com>
2024-03-04 06:30:56 +0600

    react-knowledge-cafe

---
+++ Milestone 7 Introduction to simple React/Module 42/React Knowledge Cafe/react-knowledge-cafe/src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+    //we need state for loading data
+    //array te data thakai amader empty array banate hove
+    const[blogs, setBlogs] = useState([]);
+
+    //for data load, we need useEffect
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ Milestone 7 Introduction to simple React/Module 42/React Knowledge Cafe/react-knowledge-cafe/src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl bg-red-500'>Vite + React</h1>
+        </div>
+    );
+};
+
+export default Header;
