d832dc0706a23044037494891dcee138b32344af
arifulmrislam <ariful.mr.islam@gmail.com>
2024-03-08 23:35:38 +0600

    complete the project and deploy to surge

--- Milestone 7 Introduction to simple React/Module 42/React Knowledge Cafe/react-knowledge-cafe/src/Components/Blogs/Blogs.jsx
+++ Milestone 7 Introduction to simple React/Module 42/React Knowledge Cafe/react-knowledge-cafe/src/Components/Blogs/Blogs.jsx
@@ -1,22 +1,35 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from "prop-types";
 
-const Blogs = () => {
+const Blogs = ({ handleAddToBookmark, handleMarkAsRead }) => {
     //we need state to keep data on variable
     //array te data thakai amader empty array banate hove
-    const[blogs, setBlogs] = useState([]);
+    const [blogs, setBlogs] = useState([]);
 
     //for data load, we need useEffect
-    useEffect(()=>{
-        fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    },[])
+    useEffect(() => {
+        fetch("blogs.json")
+            .then((res) => res.json())
+            .then((data) => setBlogs(data));
+    }, []);
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h1 className="text-4xl mb-3">Blogs: {blogs.length}</h1>
+            {blogs.map((blog) => (
+                <Blog key={blog.id}
+                    blog={blog}
+                    handleAddToBookmark={handleAddToBookmark}
+                    handleMarkAsRead={handleMarkAsRead}
+                ></Blog>
+            ))}
         </div>
     );
 };
 
+Blogs.propTypes = {
+    handleAddToBookmark: PropTypes.func.isRequired,
+    handleMarkAsRead: PropTypes.func.isRequired
+};
 export default Blogs;
