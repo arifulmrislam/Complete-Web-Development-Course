73922bcf7a6efb4da683a91f4e84e380ced84b6b
arifulmrislam <ariful.mr.islam@gmail.com>
2024-03-12 05:58:34 +0600

    react conceptual session

---
+++ Milestone 7 Introduction to simple React/Conceptual Sessions/React core concept/script.js
@@ -0,0 +1,18 @@
+
+// function ChangeTitle() {
+//     document.getElementById("title").innerHTML = "Programming Hero";
+// }
+
+// ChangeTitle();
+
+let minHtml = "<div><p>Hi</p></div>";
+
+minHtml = minHtml + Hello();
+
+function Hello(){
+    return "<div><p>Hello</p></div>"
+
+
+}
+
+console.log(minHt  ml);
---
+++ Milestone 7 Introduction to simple React/Conceptual Sessions/React core concept/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+let minHtml = "<div><p>Hi</p></div>";
+
+minHtml = minHtml + Hello();
+
+function Hello(){
+    return "<div><p>Hello</p></div>"
+
+
+}
+
+console.log(minHtml);
