a07abbcb0e037a325fc230e90de5ba01c7336f6c
arifulmrislam <ariful.mr.islam@gmail.com>
2024-03-20 08:09:07 +0600

    file copy from previous css file

--- Milestone 7 Introduction to simple React/Conceptual Sessions/React_core_concept2/react_concept/src/App.jsx
+++ Milestone 7 Introduction to simple React/Conceptual Sessions/React_core_concept2/react_concept/src/App.jsx
@@ -5,11 +5,13 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Main></Main>
     </>
   )
 }
 
-function Header(){
+// header component
+function Header() {
   return (
     <header>
       <div className="nav">
@@ -27,4 +29,67 @@ function Header(){
   );
 }
 
+// main component
+function Main() {
+  return (
+    <section className="main">
+      <Left></Left>
+      <Right></Right>
+    </section>
+  )
+}
+
+function Left() {
+  return (
+    <div className='left'>
+      <JsConfig></JsConfig>
+    </div>
+  )
+}
+function Right() {
+  return (
+    <div className='right'>
+      <PersonalTraining></PersonalTraining>
+      <RecentVideos></RecentVideos>
+    </div>
+  );
+}
+
+function JsConfig() {
+  return (
+    <div className="js-config">
+      <h4>JS Confusing Parts</h4>
+      <p>Understand this, scope, hoisting, closure, inheritance, bind, call, apply, prototype, event delegation, dom, timers and many more caveats.</p>
+    </div>
+  )
+}
+
+function PersonalTraining(){
+  return(
+      <div id="personal-training">
+        <h2>Personal Training for $30</h2>
+        <p>
+          If you need career guide, personal interview tips, mock interview,
+          Life Coaching, or anything else, you can book me for 30 min. Just pay
+          $30 via paypal to Codinism (my company) and then book a time slot You
+          can Book me
+        </p>
+      </div>
+  )
+}
+
+function RecentVideos(){
+  return(
+      <div id="recent-videos">
+        <h2>Recent Videos</h2>
+        <ul>
+          <li>15+ tricks for dev tool</li>
+          <li>15+ tricks for dev tool</li>
+          <li>15+ tricks for dev tool</li>
+          <li>15+ tricks for dev tool</li>
+        </ul>
+        <button className="btn-details">More on youtube</button>
+      </div>
+  )
+}
 export default App
