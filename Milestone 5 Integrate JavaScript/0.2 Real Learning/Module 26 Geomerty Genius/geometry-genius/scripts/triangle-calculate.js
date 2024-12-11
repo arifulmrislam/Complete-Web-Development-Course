a0a701e3fd8fa3ea66c0935fea87988f995b3bd8
arifulmrislam <ariful.mr.islam@gmail.com>
2024-12-12 00:36:39 +0600

    DOM geomety genius code updated

--- Milestone 5 Integrate JavaScript/0.2 Real Learning/Module 26 Geomerty Genius/geometry-genius/scripts/triangle-calculate.js
+++ Milestone 5 Integrate JavaScript/0.2 Real Learning/Module 26 Geomerty Genius/geometry-genius/scripts/triangle-calculate.js
@@ -1,10 +1,33 @@
 function calculateTriangleArea() {
+    //get triangle base and height value
     const TriangleBaseInput = document.getElementById('triangle-base');
     const TriangleHeightInput = document.getElementById('triangle-height');
     const baseText = TriangleBaseInput.value.trim();
     const heightText = TriangleHeightInput.value.trim();
     const base = parseFloat(baseText);
     const height = parseFloat(heightText);
+    //calculate triangle area
     const result = 0.5 * base * height;
     console.log(result);
+
+    //display triangle are
+    const area = document.getElementById('triangle-area');
+    area.innerText = result;
+}
+
+function calculateRectangleArea(){
+    //get triangle base and height value
+    const rectangleBaseInput = document.getElementById('rectangle-base');
+    const rectangleHeightInput = document.getElementById('rectangle-height');
+    const baseText = rectangleBaseInput.value.trim();
+    const heightText = rectangleHeightInput.value.trim();
+    const base = parseFloat(baseText);
+    const height = parseFloat(heightText);
+    //calculate triangle area
+    const result = base * height;
+    console.log(result);
+
+    //display triangle are
+    const area = document.getElementById('rectangle-area');
+    area.innerText = result;
 }
