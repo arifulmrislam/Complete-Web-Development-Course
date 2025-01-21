8f1bd33aba9a147dd2825b4deaaab642e8d8353c
arifulmrislam <ariful.mr.islam@gmail.com>
2025-01-21 09:22:59 +0600

    practice some react core concept

---
+++ Milestone 7 Introduction to simple React/Module 38 React Core Concept/react-core-concept-2 2.0/explore-react-core-concept2/src/Friend.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+
+export default function Friend({friend}) {
+    const {name, email, city} = useState(null);
+    return (
+        <div className="box">
+            <h4>Name:{friend.name}</h4>
+            <p>Email:{friend.email}</p>
+            <p>City:{friend.address.city}</p>
+        </div>
+    );
+} 
--- Milestone 7 Introduction to simple React/Module 38 React Core Concept/react-core-concept-2 2.0/explore-react-core-concept2/src/Friends.jsx
+++ Milestone 7 Introduction to simple React/Module 38 React Core Concept/react-core-concept-2 2.0/explore-react-core-concept2/src/Friends.jsx
@@ -1,5 +1,25 @@
 import './Friends.css';
+import { useState, useEffect } from 'react';
+import Friend from './friend';
 
 export default function Friends(){
-    
+    const [friends, setFriend] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setFriend(data))
+    },[]);
+
+    return ( 
+        <div className='box'>
+            <h3>Friends: {friends.length}</h3>
+            {
+            friends.map(friend => 
+                <Friend friend={friend}></Friend>
+            )
+        }
+        </div>
+
+    )
 }
