5e2437647af08ea7549deec39b481a353a167401
arifulmrislam <ariful.mr.islam@gmail.com>
2025-01-31 16:25:58 +0600

    project scribbles cafe

---
+++ Milestone 7 Introduction to simple React/Module 42  Simple React SPA with scribbles Cafe/Scribbles Cafe/react-scribbles-cafe/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+    return (
+        <>
+            <div className="text-6xl bg-red-700">
+                Hello
+            </div>
+        </>
+    );
+}
+
+export default App;
