b1a9ce4a707c70667e71df30092cfc51de776370
arifulmrislam <ariful.mr.islam@gmail.com>
2025-02-02 16:35:18 +0600

    add some functionality for 'alert'

--- Milestone 7 Introduction to simple React/Final Assignment of React 01.02.25/BPL-DREAM11/src/App.jsx
+++ Milestone 7 Introduction to simple React/Final Assignment of React 01.02.25/BPL-DREAM11/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Navbar from './components/Navbar/Navbar';
 import MainSection from './components/MainSection/MainSection';
 import Players from './components/Players/Players';
+import { ToastContainer, toast } from 'react-toastify';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
     const [coins, setCoins] = useState(0);
@@ -14,18 +16,32 @@ function App() {
         setCoins((prevCoins) => prevCoins + coin);
     };
 
-const handlePlayerAddToSelect = (player) => {
-    const { name, biddingPrice } = player; 
+    const handlePlayerAddToSelect = (player) => {
+        const { name, biddingPrice } = player;
 
-    if (coins < biddingPrice) {
-        alert(`You don't have enough coins to buy ${name}.`);
-        return;
+        if (coins < biddingPrice) {
+            toast.error(
+        <div className="toast-error">
+            <i className="fas fa-exclamation-circle"></i>
+            <p>{`You don't have enough coins to buy ${name}.`}</p>
+        </div>,
+        {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
     }
+    );
+            return;
+        }
 
-    // Deduct cost and add the player to the selected list
-    setCoins((prevCoins) => prevCoins - biddingPrice);
-    setSelectedPlayers((prevPlayers) => prevPlayers + 1);
-};
+        // Deduct cost and add the player to the selected list
+        setCoins((prevCoins) => prevCoins - biddingPrice);
+        setSelectedPlayers((prevPlayers) => prevPlayers + 1);
+    };
 
 
     return (
@@ -34,6 +50,7 @@ const handlePlayerAddToSelect = (player) => {
             <Banner handleAddToCoin={handleAddToCoin}></Banner>
             <MainSection selectedPlayer={selectedPlayer}></MainSection>
             <Players handlePlayerAddToSelect={handlePlayerAddToSelect}></Players>
+            <ToastContainer />
         </>
     )
 }
