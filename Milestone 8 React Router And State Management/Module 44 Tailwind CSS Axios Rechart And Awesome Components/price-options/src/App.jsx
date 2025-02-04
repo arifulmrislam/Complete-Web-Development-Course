7f3b0cbf26183ba018606eb34e8c0907010fb10f
arifulmrislam <ariful.mr.islam@gmail.com>
2025-02-04 06:30:02 +0600

    work on new milestone 8

---
+++ Milestone 8 React Router And State Management/Module 44 Tailwind CSS Axios Rechart And Awesome Components/price-options/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+
+
+    return (
+        <>
+            <NavBar></NavBar>
+            <h1 className='text-7xl bg-amber-300'>Vite + React</h1>
+            
+        </>
+    )
+}
+
+export default App
