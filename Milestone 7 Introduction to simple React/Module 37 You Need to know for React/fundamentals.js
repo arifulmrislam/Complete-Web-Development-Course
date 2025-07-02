891e728645ba32c29de6dd62ede8b84c4791e14c
arifulmrislam <ariful.mr.islam@gmail.com>
2025-07-02 09:25:40 -0500

    js refresher

--- Milestone 7 Introduction to simple React/Module 37 You Need to know for React/fundamentals.js
+++ Milestone 7 Introduction to simple React/Module 37 You Need to know for React/fundamentals.js
@@ -1,15 +1,85 @@
-const addToLocalStorage = () => {
-    const idInput = document.getElementById('storage-id');
-    const id = idInput.value;
-    const valueInput = document.getElementById('storage-value');
-    const value = valueInput.value;
+// const addToLocalStorage = () => {
+//     const idInput = document.getElementById('storage-id');
+//     const id = idInput.value;
+//     const valueInput = document.getElementById('storage-value');
+//     const value = valueInput.value;
+//
+//     if(id && value){
+//         localStorage.setItem(id, value);
+//         idInput.value = '';
+//         valueInput.value = '';
+//     } else {
+//         window.alert('Enter the value');
+//     }
+//
+// }
 
-    if(id && value){
-        localStorage.setItem(id, value);
-        idInput.value = '';
-        valueInput.value = '';
-    } else {
-        window.alert('Enter the value');
-    }
+// function myFunction() {
+//     var x = 10;
+//     // console.log(x);
+//     if(true){
+//         let y = 20;
+//         console.log(y);
+//     }
+//     console.log(x);
+// }
+//
+// myFunction();
+// // console.log(x);
 
-}
+//
+// let fruits = ['apple', 'banana', 'mango', 'orange'];
+//
+// // const fruits1 = fruits.slice();
+// let fruits1 = fruits.splice(1,1,'pine-apple'); //new array created
+//
+// console.log(fruits);
+// console.log(fruits1);
+
+// let joinMethod = [10,20,30,40];
+
+// let joinMethod1 = joinMethod.join();
+// console.log(joinMethod1);
+
+// let joinMethod = [10, 20, 30, 40];
+//
+// const sum = joinMethod.reduce((accumulator, currentValue) => accumulator + currentValue);
+// console.log(sum);
+
+// const numbers = [1, 2, 3, 4, 5];
+//
+// const doubledNumbers = numbers.findIndex(num => num === 5);
+// console.log(`Index number: ${doubledNumbers}`);
+
+// let randomNumbers = Math.floor(Math.random() * 10) + 1;
+//
+// while (randomNumbers !== 10){
+//     console.log(randomNumbers);
+//     randomNumbers = Math.floor(Math.random() * 10) + 1;
+// }
+
+// var argumentsLength = function(...args){
+//     let items = args[0];
+//     console.log(items.length);
+// }
+//
+// argumentsLength([{}, null, '3']);
+
+// const person = {
+//     name: 'John Doe',
+//     age: 30,
+//     hobbies: ['reading', 'writing', 'coding'],
+//     friends: [
+//         {
+//             name: 'Jane Doe',
+//             age: 25,
+//         },
+//         {
+//             name: 'John Smith',
+//             age: 35,
+//         },
+//     ],
+// };
+// 
+// console.log(person.hobbies);
+
