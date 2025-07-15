8e189e864d3cc063bd799448915b9beb36a75737
arifulmrislam <ariful.mr.islam@gmail.com>
2025-07-15 11:55:29 -0500

    🔥

---
+++ Milestone 009  React Authentication/Module 49 React Auth/simple-firebase/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Hello form Header!</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ Milestone 009  React Authentication/Module 49 React Auth/simple-firebase/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Hello From Home!! And I love to do this project.</h2>
+        </div>
+    );
+};
+
+export default Home;
