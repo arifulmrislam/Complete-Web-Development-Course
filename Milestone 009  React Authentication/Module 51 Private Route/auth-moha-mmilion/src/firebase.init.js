e020d6adf274bc195ef65b3cab7a8202eee4bb1b
arifulmrislam <ariful.mr.islam@gmail.com>
2025-07-20 03:18:10 -0500

    set up a react project with react router

---
+++ Milestone 009  React Authentication/Module 51 Private Route/auth-moha-mmilion/src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBswbQRXfBcgblzrUoz_7q0bMC-_qnbLSU',
+  authDomain: 'auth-moha-milon-c7c18.firebaseapp.com',
+  projectId: 'auth-moha-milon-c7c18',
+  storageBucket: 'auth-moha-milon-c7c18.firebasestorage.app',
+  messagingSenderId: '888222169848',
+  appId: '1:888222169848:web:f8cbaabb38b9dbc63cd90d',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- Milestone 009  React Authentication/Module 51 Private Route/auth-moha-mmilion/src/main.jsx
+++ Milestone 009  React Authentication/Module 51 Private Route/auth-moha-mmilion/src/main.jsx
@@ -2,9 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+        {
+            path: '/',
+            element: <Home/>
+        },
+        {
+            path: 'login',
+            element: <Login/>
+        },
+        {
+            path: 'register',
+            element: <Register/>
+        }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
