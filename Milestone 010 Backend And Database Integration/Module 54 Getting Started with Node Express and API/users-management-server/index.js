e050d9659102d7862e3fd64a2f4f64c825560810
arifulmrislam <ariful.mr.islam@gmail.com>
2025-08-09 22:26:36 -0500

    mongodb setup done

--- Milestone 010 Backend And Database Integration/Module 54 Getting Started with Node Express and API/users-management-server/index.js
+++ Milestone 010 Backend And Database Integration/Module 54 Getting Started with Node Express and API/users-management-server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
 
 const users = [
     {id: 1, name:'Sabana', email:'sabana@gmail.com'},
@@ -10,11 +14,16 @@ const users = [
 
 app.get('/', (req, res)=> {
     res.send('Users Management server is running')
-})
+});
 
 app.get('/users', (req, res)=> {
     res.send(users);
 })
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
+});
+
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
