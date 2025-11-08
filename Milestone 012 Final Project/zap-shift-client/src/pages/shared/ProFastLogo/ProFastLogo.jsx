45e17a60f24fa6546b3c0464d3a787a1e6948337
arifulmrislam <ariful.mr.islam@gmail.com>
2025-11-08 11:18:32 -0600

    React Layout Components RootLayout, Navbar, Footer

---
+++ Milestone 012 Final Project/zap-shift-client/src/pages/shared/ProFastLogo/ProFastLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../../assets/logo.png'
+
+const ProFastLogo = () => {
+    return (
+        <div>
+            <img src={logo} alt=''></img>
+        </div>
+    );
+};
+
+export default ProFastLogo;
